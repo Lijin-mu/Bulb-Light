8ed51931ca455e0c07265a783880b362987b3da2
lijin2hats <lijin@2hatslogic.com>
2022-10-13 16:35:35 +0530

    code improvement

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,28 +4,42 @@ let bulbOn = document.querySelector('[data-bulb-status=on]');
 let bulbOnBtn = document.querySelector('[data-bulb-trigger=ON]');
 let bulbOffBtn = document.querySelector('[data-bulb-trigger=OFF]');
 let bulbBlinkBtn = document.querySelector('[data-bulb-trigger=BLINK]');
+var intervalarray = [];
+var interval=null;
 
 // methods
 
 var bulb = {
-    switch: function(status){
-
-        if(status === true) {
+   
+    switch:function(task){
+        
+        if(task === true) {
+            for (var id of intervalarray) {
+                this.clear(id);
+            }
+            intervalarray=[];
             bulbItem.setAttribute("data-bulb-status", "on"); 
-        } else {
+        } else if(task === false){
+            for (var id of intervalarray) {
+                this.clear(id);
+            }
+            intervalarray=[];
             bulbItem.setAttribute("data-bulb-status", "off"); 
+        } else{
+            interval = setInterval(() => {
+                bulbItem.setAttribute("data-bulb-status", "on"); 
+                setTimeout(() => { bulbItem.setAttribute("data-bulb-status", "off");  }, 200);
+            }, 300);
+            intervalarray.push(interval);
         }
     },
-    blink:function(){
-        setInterval(() => {
-            this.switch(true);
-            setTimeout(() => { this.switch(false); }, 200);
-        }, 300);
+    clear:function(id){
+        clearInterval(id);
     },
     init:function(){
         bulbOnBtn.addEventListener("click", function(){ bulb.switch(true) });
         bulbOffBtn.addEventListener("click", function(){ bulb.switch(false) });
-        bulbBlinkBtn.addEventListener("click", function(){ bulb.blink() } );
+        bulbBlinkBtn.addEventListener("click", function(){ bulb.switch() } );
     }
 }
 
